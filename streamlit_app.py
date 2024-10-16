a6a15c787dd4c315b3b5a72b4d2bfda957783efa
shigepon0217 <chanshige0127@gmail.com>
2024-10-16 10:30:42 +0000

    Langchainの設定追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 import time
 import requests
+import os
 
 API_TOKEN="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"
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-3.2-1B"
+
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["LANGCHAIN_PROJECT"]="pr-artistic-town-49"
 
 st.title("🎈 My new StreamLit app")
 
